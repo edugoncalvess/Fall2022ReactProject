3ee8a2650f74e548c5a941f46d749eacb483e631
edugoncalvess <68749343+edugoncalvess@users.noreply.github.com>
2022-11-29 11:53:03 -0800

    Implementing React Router in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 // import logo from './logo.svg';
 // import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Products from './components/Products';
 import RickMorty from './components/RickMorty';
 import ProjLog from './components/ProjLog';
 
-
-
-
 function App() {
-
   return (
     <section id="app">
       <div className="container">
         <Navbar />
-        <Products />
-        {/* <RickMorty  /> */}
-        {/* <ProjLog /> */}
+        <Routes>
+          <Route path="/" element={<Products />}></Route>
+          <Route exact path="/products" element={<Products />}></Route>
+          <Route exact path="/rickmorty" element={<RickMorty />}></Route>
+          <Route exact path="/projLog" element={<ProjLog />}></Route>
+        </Routes>
       </div>
     </section>
   );
