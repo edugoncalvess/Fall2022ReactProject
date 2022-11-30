f46aaead76d1ba63827683296ebcf5ddb61bd492
edugoncalvess <68749343+edugoncalvess@users.noreply.github.com>
2022-11-29 20:48:43 -0800

    some adjustments in the App description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,13 +6,16 @@ const Home = () => {
             <Header title={'Home'} />
             <br />
             <h3>Project Description</h3>
-            <p>Something here</p>
+            <p>This page displays a React Js application that uses create-react framework</p>
             <p>The source code for this project can be found at the following link:</p>
             <a href='https://github.com/edugoncalvess/Fall2022ReactProject'>Fall2022ReactProject source code</a>
-            <p>etc....</p>
+            <p>
+                The live version of this code is available on the following link
+            </p>
+            <a href='https://fall2022-react-project.vercel.app/'>https://fall2022-react-project.vercel.app/</a>
             <br />
             <h3>Rick & Morty Simple App</h3>
-            <p>The version of the Rick and Morty page here uses react components and imports a json file as a parameter</p>
+            <p>The version of the Rick and Morty page here uses react components and imports a json file as a parameter into create-react framework</p>
             <p>A simpler version can be founded in the following link</p>
             <a href='https://edugoncalvess.github.io/BasicAppRickMorty/'>https://edugoncalvess.github.io/BasicAppRickMorty/</a>
             <p>Its source code can be shown here:</p>
--- src/components/ProjLog.jsx
+++ src/components/ProjLog.jsx
@@ -9,7 +9,7 @@ const ProjLog = () => {
             <p>2022-11-10 - Group Creation</p>
             <p>etc....</p>
             <p>2022-11-27 - Zoo API is shutdown and the project has to be reshaped to use fakestore API</p>
-            <p>etc....</p>
+            <p>2022-11-29 - React Router is implemented. A live version of the App is created.</p>
             <br />
             <br />
             <h3>Future Implementations/Improvements</h3>
