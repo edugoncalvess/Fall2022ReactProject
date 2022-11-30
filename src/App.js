f7d6f6d1a50563baffcfd650dc15967b1d54ad13
edugoncalvess <68749343+edugoncalvess@users.noreply.github.com>
2022-11-29 17:40:49 -0800

    Adding Home to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-// import logo from './logo.svg';
 // import './App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 import Navbar from './components/Navbar';
 import Products from './components/Products';
 import RickMorty from './components/RickMorty';
 import ProjLog from './components/ProjLog';
 
+
+
+
 function App() {
+
   return (
+
     <section id="app">
       <div className="container">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Products />}></Route>
+          <Route path="/" element={<Home />}></Route>
           <Route exact path="/products" element={<Products />}></Route>
           <Route exact path="/rickmorty" element={<RickMorty />}></Route>
           <Route exact path="/projLog" element={<ProjLog />}></Route>
         </Routes>
       </div>
     </section>
+
   );
 }
 
