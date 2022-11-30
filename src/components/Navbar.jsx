fe335f3581ed13f2c48aacab8bc8af029b01fb0c
edugoncalvess <68749343+edugoncalvess@users.noreply.github.com>
2022-11-29 17:41:44 -0800

    Updating Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,25 @@ import { Link } from 'react-router-dom';
 const Navigation = () => {
     return (
       <nav className="navbar navbar-expand-lg sticky-top navbar-light bg-dark">
-        <div className="container-fluid">
-          <Link className="navbar-brand text-light" to="#">React Project</Link>
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link text-light" aria-current="page" to="/">Products</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-light" to="/rickmorty">Rick and Morty</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-light" to="/projLog">Project Log</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+
+      <div className="container-fluid">
+        <Link className="navbar-brand text-light" to="#">React Project</Link>
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <Link className="nav-link text-light" aria-current="page" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-light" aria-current="page" to="/products">Products</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-light" to="/rickmorty">Rick and Morty</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-light" to="/projLog">Project Log</Link>
+          </li>
+        </ul>
+      </div>
+        </nav>
     )
 }
 
