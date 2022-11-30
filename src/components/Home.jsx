43ed0ded6dfaaff558cfec4de35310c24357294f
edugoncalvess <68749343+edugoncalvess@users.noreply.github.com>
2022-11-30 09:31:30 -0800

    Adding API links to Home component
    To add the API source code and endpoint links for documentation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
         <div>
             <Header title={'Home'} />
             <br />
+            
             <h3>Project Description</h3>
             <p>This page displays a React Js application that uses create-react framework</p>
             <p>The source code for this project can be found at the following link:</p>
@@ -14,6 +15,7 @@ const Home = () => {
             </p>
             <a href='https://fall2022-react-project.vercel.app/'>https://fall2022-react-project.vercel.app/</a>
             <br />
+            <br />
             <h3>Rick & Morty Simple App</h3>
             <p>The version of the Rick and Morty page here uses react components and imports a json file as a parameter into create-react framework</p>
             <p>A simpler version can be founded in the following link</p>
@@ -21,9 +23,14 @@ const Home = () => {
             <p>Its source code can be shown here:</p>
             <a href='https://github.com/edugoncalvess/BasicAppRickMorty.git'>BasickAppRickMorty source code</a>
             <br />
-            <h3>API documentation</h3>
+            <br />
+            <h3>Quotes API</h3>
 
-            <p>Here we describe the endpoints of our API</p>
+            <p>The source code for the API can be found at the following link:</p>
+            <a href='https://github.com/raditzanardi/quotesAPI.git'>Quotes API source code</a>
+            <p>The endpoint Url is: </p>
+            <a href='https://radit-api.herokuapp.com/quotes'>https://radit-api.herokuapp.com/quotes</a>
+
  
         </div>
     )
