aa7455cdbe992d766da1f3a5ee0f906cad4c38ef
raditzanardi <radit2639@gmail.com>
2022-11-30 16:20:13 -0800

    Added search bar to quotes API

--- src/components/APIPage.jsx
+++ src/components/APIPage.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect} from "react";
 import APICard from './APICard';
 import Header from './Header';
+import { Input } from 'semantic-ui-react'
 
 const APIPage = () => {
     const [quotes, setQuotes] = useState([]);
+    const [filteredResults, setFilteredResults] = useState([])
+    const [searchInput, setSearchInput] = useState('');
 
     useEffect(() => {        
         fetch('https://whispering-atoll-67985.herokuapp.com/https://radit-api.herokuapp.com/quotes')
@@ -13,19 +16,48 @@ const APIPage = () => {
         });
     }, []);
 
-    return (
+    const searchItems = (searchValue) => {
+        setSearchInput(searchValue)
+        if (searchInput !== '') {
+            const filteredData = quotes.filter((item) => {
+                return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
+            })
+            setFilteredResults(filteredData)
+            console.log(filteredData)
+        }
+        else{
+            setFilteredResults(quotes)
+        }
+    }
+
+    return(
         <div>
             <Header title={'Quotes API'} />
+            <Input icon='search'
+                placeholder='Search...'
+                onChange={(e) => searchItems(e.target.value)}
+            />
             <div className="row">
-            {quotes.map((eachQuote) => (
-                <APICard 
-                 quotes={eachQuote}
-                 key={eachQuote._id}/>
-            ))}
+                {searchInput.length > 1 ? (
+                    filteredResults.map((eachQuote) => {
+                        return (
+                            <APICard 
+                             quotes={eachQuote}
+                             key={eachQuote._id}/>
+                        )
+                    })
+                ) : (
+                    quotes.map((eachQuote) => {
+                        return (
+                            <APICard 
+                             quotes={eachQuote}
+                             key={eachQuote._id}/>
+                        )
+                    })
+                )}
+            </div>
         </div>
-        </div>
-
-      );
+    )
 
 }
 
